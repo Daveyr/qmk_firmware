aec60d846ebe22c45769c9a8fe5a1f08ed0904fd
Richard <davey.richard@gmail.com>
2021-01-03 21:15:45 +0000

    basic working layer status on oled

--- keyboards/tiki/keymaps/default/keymap.c
+++ keyboards/tiki/keymaps/default/keymap.c
@@ -174,6 +174,35 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
   return rotation;
 }
 
+#ifdef OLED_DRIVER_ENABLE
+void oled_task_user(void) {
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("Layer: "), false);
+
+    switch (get_highest_layer(layer_state)) {
+        case _QWERTY:
+            oled_write_P(PSTR("Default\n"), false);
+            break;
+        case _LOWER:
+            oled_write_P(PSTR("FN\n"), false);
+            break;
+        case _RAISE:
+            oled_write_P(PSTR("ADJ\n"), false);
+            break;
+        default:
+            // Or use the write_ln shortcut over adding '\n' to the end of your string
+            oled_write_ln_P(PSTR("Undefined"), false);
+    }
+
+    // Host Keyboard LED Status
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+}
+#endif
+
+
 // When you add source files to SRC in rules.mk, you can use functions.
 //const char *read_layer_state(void);
 //const char *read_logo(void);
@@ -194,7 +223,7 @@ static void render_logo(void) {
     };
     oled_write_P(qmk_logo, false);
     }
-*/
+
 void oled_task_user(void) {
   if (is_keyboard_master()) {
     // If you want to change the display of OLED, you need to change here
@@ -211,7 +240,7 @@ void oled_task_user(void) {
     
 }
   }
-  
+*/
 #endif // OLED_DRIVER_ENABLE
 /*
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
