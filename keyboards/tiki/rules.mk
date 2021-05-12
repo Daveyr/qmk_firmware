3ac35485aed9af6bb221d56aa33578a48dcfa6ab
Daveyr <26368709+Daveyr@users.noreply.github.com>
2021-05-12 09:00:23 +0100

    Update rules.mk
    New rules: debounce, NKRO, extrakey (for the audio mute button)

--- keyboards/tiki/rules.mk
+++ keyboards/tiki/rules.mk
@@ -2,4 +2,7 @@ MCU = atmega32u4
 BOOTLOADER = caterina
 SPLIT_KEYBOARD = yes
 DEFAULT_FOLDER = tiki/rev1
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+NKRO_ENABLE = yes           # USB Nkey Rollover
 OLED_DRIVER_ENABLE = yes    # OLED display
+DEBOUNCE_TYPE = eager_pk
