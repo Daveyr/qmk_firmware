922c306df18e23cee5dc7a0abc0602afc2a599d6
Richard <davey.richard@gmail.com>
2021-06-29 20:59:32 +0100

    long term stable changes to Tiki

--- keyboards/tiki/rules.mk
+++ keyboards/tiki/rules.mk
@@ -4,4 +4,4 @@ SPLIT_KEYBOARD = yes
 DEFAULT_FOLDER = tiki/rev1
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 NKRO_ENABLE = yes           # USB Nkey Rollover
-OLED_DRIVER_ENABLE = yes    # OLED display
+OLED_DRIVER_ENABLE = no    # OLED display
